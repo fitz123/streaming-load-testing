e9147211f82eaf99563a16a5c901669b883415a3
Your Name <you@example.com>
2023-07-05 14:40:57 +0700

    unique users

--- load_generator/locustfiles/hls_player.py
+++ load_generator/locustfiles/hls_player.py
@@ -18,6 +18,9 @@ import resource
 import sys
 import random
 import logging
+import hashlib
+import base64
+import uuid
 
 if sys.version_info[0] < 3:
     raise Exception("Must be using Python 3")
@@ -29,7 +32,7 @@ resource.setrlimit(resource.RLIMIT_NOFILE, resource.getrlimit(
     resource.RLIMIT_NOFILE)
 )
 
-MANIFEST_FILE = os.getenv('MANIFEST_FILE')
+CHANNEL_URI = os.getenv('CHANNEL_URI')
 HOST_PORT = os.getenv('HOST_PORT')
 
 class PlayerTaskSet(TaskSet):
@@ -44,18 +47,28 @@ class PlayerTaskSet(TaskSet):
     everything into a single name
     """
     def on_start(self):
+        # Calculate ticket and user_id
+        secret = 'secret'  # replace with your actual secret
+        self.user_id = str(uuid.uuid4()).lower()
+        ticket_string = f"9999999999./{CHANNEL_URI}.{secret}.{self.user_id}"
+        self.ticket = base64.urlsafe_b64encode(hashlib.md5(ticket_string.encode()).digest()).rstrip(b'=').decode('utf-8')
+        logger.debug(f"Ticket string is: {ticket_string}")
+        logger.debug(f"Ticket is: {self.ticket}")
+
         # Get the base URL from the MANIFEST_FILE
-        base_url = (f"{MyLocust.host}:{HOST_PORT}/{MANIFEST_FILE.rsplit('/', 1)[0]}")
+        base_url = (f"{MyLocust.host}:{HOST_PORT}/v4/{self.ticket}/9999999999/{self.user_id}/{self.ticket}/9999999999/{CHANNEL_URI}")
 
         # get master manifest
-        master_url = f"{base_url}/{MANIFEST_FILE.rsplit('/', 1)[1]}"
+        master_url = f"{base_url}/playlist.m3u8"
+        logger.debug(f"Master Manifest File URL: {master_url}")
         master_m3u8 = self.client.get(master_url, name="playlist", verify=False)
         self.parsed_master_m3u8 = m3u8.M3U8(content=master_m3u8.text, base_uri=base_url)
+        logger.debug(f"parsed_master_m3u8 playlists: {self.parsed_master_m3u8.playlists[0]}")
 
     @task(1)
     def play_stream(self):
         # Get the base URL from the MANIFEST_FILE
-        base_url = (f"{MyLocust.host}:{HOST_PORT}/{MANIFEST_FILE.rsplit('/', 1)[0]}")
+        base_url = (f"{MyLocust.host}:{HOST_PORT}/v4/{self.ticket}/9999999999/{self.user_id}/{self.ticket}/9999999999/{CHANNEL_URI}")
 
         # get the chunks URI from the master manifest
         variant_uri = self.parsed_master_m3u8.playlists[0].uri
